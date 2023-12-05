3f309a71b8dc5ef9c3ec3803cd02d2157c944118
brenoharaujo2503 <brenoharaujomorais@gmail.com>
2023-12-05 19:16:24 -0300

    feat: create Nokia and iphone integration

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,25 +2,29 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public string Number { get; set; }
+        private string Model { get; set; }
+        private string Imei { get; set; }
+        private int Memory { get; set; }
+        
+        public Smartphone(string number, string model, string imei, int memory)
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Number = number;
+            Model = model;
+            Imei = imei;
+            Memory = memory;
         }
 
-        public void Ligar()
+        public void TurnOn()
         {
             Console.WriteLine("Ligando...");
         }
 
-        public void ReceberLigacao()
+        public void ReceiveCall()
         {
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public abstract void InstallApp(string appName);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(number: "123456", model: "Model 01", imei: "1111111", memory: 32);
+nokia.TurnOn();
+nokia.InstallApp("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(number: "654321", model: "Model 02", imei: "2222222", memory: 16);
+iphone.TurnOn();
+iphone.InstallApp("Telegram");
